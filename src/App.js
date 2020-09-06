5b2b9ec366854be17813e64b1ad00609cf8279ef
Reggie Littleton <Hitsupod@gmail.com>
2020-09-06 13:36:40 -0500

    Setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import $ from 'jquery';
+import About from './components/About';
+import Contact from './components/Contact';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Resume from './components/Resume';
+import Work from './components/Work';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  constructor(props) {
+
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return(
+
+      <div className = "App">
+        <Header />
+        <About />
+        <Work />
+        <Resume />
+        <Contact />
+        <Footer />
+      </div>
+    );
+  }
 }
+
 
 export default App;
