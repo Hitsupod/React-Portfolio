0cf29be546a2a7363fd6f3b0ed2e54eb97636001
Reggie Littleton <Hitsupod@gmail.com>
2020-09-06 20:56:29 -0500

    Adding Images

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,27 @@
 // import React, { Component } from 'react';
 import React from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Image from 'react-bootstrap/Image';
+import Col from 'react-bootstrap/Col';
+import Picture from '../../public/images/'
+
 
 function About() {
     return (
         <div>
-            <h1>Hey guys! I'm a About</h1>
+            <Jumbotron fluid>
+                <div>
+                <Col xs={6} md={4}>
+                    <Image src="../../public/images/DSC_0163.jpg" roundedCircle />
+                </Col>
+                    <h1>I'm Reggie Littleton.</h1>
+                    <p>
+                    I am looking to help businesses build responsive 
+                    full scale websites while optimizing user-focused 
+                    for customers with various business needs. 
+                    </p>
+                </div>
+            </Jumbotron>
         </div>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,20 @@
 // import React, { Component } from 'react';
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
 
 
 function Header() {
     return (
         <div>
-            <h1>Hey guys! I'm the Header</h1>
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="#home">Hitsupod</Navbar.Brand>
+                <Nav className="navbar">
+                <Nav.Link href="#about">About</Nav.Link>
+                <Nav.Link href="#resume">Resume</Nav.Link>
+                </Nav>
+            </Navbar>
         </div>
     )
 }
